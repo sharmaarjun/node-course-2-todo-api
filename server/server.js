dd61c1f97bfc096041739e43eb6c8fce74cf0434
arjun sharma <sharmaarjun2850@gmail.com>
2017-11-12 20:00:34 +0530

    Patch /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
-var {ObjectID} = require('mongodb');
+//var {User} = require('./models/user');
+
 
 
 var app = express();
@@ -80,10 +82,31 @@ Todo.findByIdAndRemove(id).then((todo) => {
     res.status(400).send()
 });
 });
-// app.listen(3000, () => {
-//     console.log('Started on 3000');
-// });
-//-- Changes for Heroku--
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {                   // if things dont go well we use catch
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Starting server on Port ${port}`);
 });
